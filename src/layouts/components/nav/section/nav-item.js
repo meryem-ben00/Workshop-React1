3ceea04c3b04e7f92b68ffea99c130cd7656b508
abdellahsaber07 <abdellah.saber@metave.us>
2023-11-23 17:08:03 +0100

    fix classes

--- src/layouts/components/nav/section/nav-item.js
+++ src/layouts/components/nav/section/nav-item.js
@@ -4,15 +4,16 @@ import PropTypes from "prop-types";
 export default function NavItem({ item, active }) {
   const { title, path } = item;
 
+  const activeClass = active ? "active" : "";
+
   return (
     <>
       <Link
         to={path}
-        className={
-          active
-            ? "active text-white  px-3 py-2 text-sm font-medium flex justify-center items-center navItem"
-            : " text-white px-3 py-2 text-sm font-medium flex justify-center items-center navItem"
-        }
+        className={[
+          " text-white  px-3 py-2 text-sm font-medium flex justify-center items-center navItem",
+          activeClass,
+        ].join(" ")}
         aria-current="page"
       >
         {title}
