4d58c18d8faebcb4cc333bd82ead9fc8e163dd3e
ahmed-ben-chakhter <ahmed.benchakhter@metave.us>
2023-11-26 14:25:28 +0100

    Create and style the Doctors table.

--- src/pages/users/doctors-users/doctors-table.js
+++ src/pages/users/doctors-users/doctors-table.js
@@ -1,5 +1,5 @@
-import { useReactTable } from "@tanstack/react-table";
-import { useMemo } from "react";
+import { flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { useState } from "react";
 import doctorsMockData from 'src/assets/mock-data/doctors-mock-data.json';
 
 export default function DoctorsTable(){
@@ -16,13 +16,68 @@ export default function DoctorsTable(){
     },
   */
 
-  const tableData =  useMemo( () => doctorsMockData, [] );
+  // const tableData =  useMemo( () => doctorsMockData, [] );
+  const [tableData, useTableData] =  useState(doctorsMockData);
+
+    function cellValue(cell){
+      console.log('Check', cell.id);
+      if(cell.column.columnDef.Headers == "Name"){
+        const cellOject = JSON.parse(cell.getValue())
+        return(
+          <>
+            <img src={cellOject.imageUrl} alt="User image" className="rounded-md" />
+            <div>
+              <span className="block">{cellOject.name}</span>
+              <span className="block userType">Doctor</span>
+            </div>
+          </>
+        )
+      }
+      if(cell.column.columnDef.Headers == " "){
+        return <img src="/src/assets/icons/rightArrowIcon.svg" alt="Right arrow." />
+      }
+
+      if(cell.column.columnDef.Headers == "Date added"){
+        const cellOject = JSON.parse(cell.getValue())
+        return(
+          <>
+            <span className="block">{cellOject.date}</span>
+            <span className="block timeAdded">{cellOject.time}</span>
+          </>
+        )
+      }
+
+      if(cell.column.columnDef.Headers == "Date added"){
+        const cellOject = JSON.parse(cell.getValue())
+        return(
+          <>
+            <span className="block">{cellOject.date}</span>
+            <span className="block timeAdded">{cellOject.time}</span>
+          </>
+        )
+      }
+
+      else return flexRender(cell.column.columnDef.cell, cell.getContext())
+  }
 
   /** @type import("@tanstack/react-table").columnDef<any> */
   const tableColumns = [
     {
       Headers: 'Name',
-      accessorKey: 'name'
+      id: 'Name',
+      accessorFn: row => `{"name": "${row.name}", "imageUrl": "${row['image-url']}"}`,
+      cell : row => {
+        const cellOject = JSON.parse(row.getValue())
+        return(
+          <>
+            <img src={cellOject.imageUrl} alt="User image" className="rounded-md" />
+            <div>
+              <span className="block">{cellOject.name}</span>
+              <span className="block userType">Doctor</span>
+            </div>
+          </>
+        )
+      } 
     },
     {
       Headers: 'ID',
@@ -38,41 +93,98 @@ export default function DoctorsTable(){
     },
     {
       Headers: 'Date added',
-      accessorKey: 'date-added'
+      id: 'Date added',
+      accessorFn: row => `{"date": "${row['date-added']}", "time": "${row['time-added']}"}`,
+      cell : row => {
+        const cellOject = JSON.parse(row.getValue())
+        return(
+          <>
+            <span className="block">{cellOject.date}</span>
+            <span className="block timeAdded">{cellOject.time}</span>
+          </>
+        )
+      }  
     },
     {
       Headers: 'Status',
-      accessorKey: 'status'
+      accessorKey: 'status',
+      cell : (row) =>(
+        <div className={
+          "bg-white py-1 px-2 rounded-md font-medium text-xs " + 
+          (
+            row.getValue() == "Approved" ? 
+              "text-[#568D4E]":
+              "text-[#F64E60]"
+          )
+        }>
+          {row.getValue()}
+        </div>          
+      )
     },
     {
-      Headers: '',
-      accessorKey: 'image-url'
-    },
-  ]
+      Headers: " ",
+      id:" ",
+      cell : (row) =>(
+        <div className="bg-white py-1 px-2 rounded-md">
+          <img className="w-5 h-5" src="/src/assets/icons/rightArrowIcon.svg" />
+        </div>          
+      )
 
-  const table = useReactTable(tableData, tableColumns);
+    }
+  ];
+
+  const table = useReactTable({
+    data: tableData, 
+    columns: tableColumns,
+    getCoreRowModel: getCoreRowModel()
+  });
 
   return (
     <div className="doctorsTableCom">
 
-      <table>
-
-        <thead>
-          <tr>
-            <th>ID</th>
-          </tr>
+      <table className="usersTable">
+        <thead className="usersTableHeader bg-white rounded-md">
+          { table.getHeaderGroups().map(
+            headerGroup => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map(
+                  header => <th key={header.id} className="text-left font-semibold text-xs py-2 px-4">
+                    {flexRender(header.column.columnDef.header, header.getContext())}
+                  </th>
+                )}
+              </tr>
+            )
+          )}
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-          </tr>
+          {table.getRowModel().rows.map(
+            row => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map(
+                  cell => (
+                    <td key={cell.id} className={
+                      "text-sm text-left py-2 font-semibold " +
+                      (
+                        cell.column.columnDef.Headers === "Name" ?
+                          "firstColumn flex items-center gap-3" 
+                        :
+                          "normalColumn px-4 "
+                      ) +
+                      (
+                        cell.column.columnDef.Headers === " "? 
+                          "text-right pr-0" 
+                        : 
+                          ""
+                      ) 
+                    } >
+                      { flexRender(cell.column.columnDef.cell, cell.getContext()) } 
+                    </td>
+                  )
+                )}
+              </tr>
+            ) 
+          )}
         </tbody>
-        <tfoot>
-          <tr>
-          <td>ID</td>
-          </tr>
-        </tfoot>
-
       </table>
 
     </div>
